7e764416497d11d2ab262281cac012d75028d89a
cpumul4 <muroykk@is.s.u-tokyo.ac.jp>
2012-10-30 20:56:29 +0900

    発行命令数増加 (1024*20)

--- simulator/memory.hpp
+++ simulator/memory.hpp
@@ -9,7 +9,7 @@
 #define INTREG_NUM   32
 #define FLOATREG_NUM 32		// rst命令でireg = fregを仮定している
 #define RAM_SIZE  1024*1024
-#define ROM_SIZE  1024*11
+#define ROM_SIZE  1024*20
 
 /* register */
 #define ZR ireg[0]
