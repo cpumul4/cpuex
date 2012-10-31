25a845868503692600bc635d21f00f921f569072
cpumul4 <muroykk@is.s.u-tokyo.ac.jp>
2012-10-31 21:00:08 +0900

    core ver1.1

---
+++ core1_1/sim/use-msim.sh
@@ -0,0 +1,2 @@
+export PATH=${HOME}/modelsim/modeltech/bin:$PATH
+export LM_LICENSE_FILE=1717@133.11.27.150
---
+++ core1_1/use-ise.sh
@@ -0,0 +1,2 @@
+source ${HOME}/Xilinx/13.4/ISE_DS/settings64.sh
+export XILINXD_LICENSE_FILE=2100@133.11.27.150
