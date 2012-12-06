e66d700c5bb2e663f59d52362da0237a71073593
cpumul4 <muroykk@is.s.u-tokyo.ac.jp>
2012-12-06 12:32:21 +0900

    symlinkのままにすることにした

--- simulator/fpu.h
+++ simulator/fpu.h
@@ -1,15 +1,1 @@
-typedef union _ieee_f {
-  float f;
-  struct _float {
-    unsigned fraction:23;
-    unsigned exp:8;
-    unsigned sign:1;
-  } b;
-} ieee_f;
-
-float fadd(float f1,float f2);
-float fmul(float f1,float f2);
-float finv(float f1);
-float sqrt_m(float f1);
-int eq_f(float f1,float f2);
-int lte_f(float f1,float f2);
+../2nd_fpu/fpu.h
