80ed362fcc8c7980889bd2329b896c6356aec3bc
cpumul4 <muroykk@is.s.u-tokyo.ac.jp>
2012-12-11 17:54:28 +0900

    simulatorのコードの再利用

---
+++ assembler/2ndassembler.cpp
@@ -0,0 +1,8 @@
+#include "./instruction.hpp"
+#include "./memory.hpp"
+
+extern int decode(char*, instr[ROM_SIZE]);
+
+void opcode_to_fnc(opcode opcode, int opc, int fnc){
+
+}
---
+++ assembler/memory.hpp
@@ -0,0 +1,1 @@
+../simulator/memory.hpp
