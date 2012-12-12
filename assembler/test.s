e6179eb16578e5665afc3f1beb948be3a89d7760
cpumul4 <muroykk@is.s.u-tokyo.ac.jp>
2012-12-12 13:55:47 +0900

    assemblerのテスト用のコード

--- assembler/test.s
+++ assembler/test.s
@@ -1,78 +1,94 @@
-	add  $r4 $r1 $r2
-	sub  $r4 $r1 $r2
-<<<<<<< HEAD
-=======
-	fadd  $r4 $r1 $r2
-	fsub  $r4 $r1 $r2
-	fmul  $r4 $r1 $r2
-	fadda  $r4 $r1 $r2
-	fsuba  $r4 $r1 $r2
-	fmula  $r4 $r1 $r2
-	faddn  $r4 $r1 $r2
-	fsubn  $r4 $r1 $r2
-	fmuln  $r4 $r1 $r2
-
-	finv  $r4 $r1		;適当に追加した。怪しい
-	finva  $r4 $r1
-	finvn  $r4 $r1
-	fabs	$f4 $f1
-	fneg	$f4 $f1
-	
-	divf  $r4 $r1 $r2
-	sqrt  $r4 $r1
->>>>>>> master
-	addi  $r4 $r1  31
-	subi  $r4 $r1  31
-
-	fadd  $r4 $r1 $r2
-	fsub  $r4 $r1 $r2
-	fmul  $r4 $r1 $r2
-	
-	fdiv  $r4 $r1 $r2
-	sqrt  $r4 $r1
-	and  $r4 $r1 $r2
-	or   $r4 $r1 $r2
-	nor  $r4 $r1 $r2
-	xor  $r4 $r1 $r2
-	sll  $r4 $r1  15
-	srl  $r4 $r1  15
-	sra  $r4 $r1  15
-	cmp  $r4 $r1 $r2
-	cmpf $r4 $r1 $r2
-	r2r  $r4 $r1
-	f2f  $r4 $r1
-	r2f  $r4 $r1
-	f2r  $r4 $r1
-	itof	$f4 $r1
-	ftoi	$r4 $f1
-	floor	$f4 $f1
-	lui  $r4 $r1  31
-	lli  $r4 $r1  31
-	flui  $r4 $r1  31
-	flli  $r4 $r1  31
-	lw	$r4 $r1 $r2
-	sw	$r4 $r1 $r2
-	lwi	$r4 $r1 31
-	swi	$r4 $r1 31
-	flw  $r4 $r1 $r2
-	fsw  $r4 $r1 $r2
-	flwi  $r4 $r1 31
-	fswi  $r4 $r1 31
-
-	in $r31
-	fin $r31
-	
-	outa $r31
-	outb $r31
-	outc $r31
-	outd $r31
-	fouta $r31
-	foutb $r31
-	foutc $r31
-	foutd $r31
-aaa:	
-	j aaa
-	jl aaa
-	jr $r31
-	jlr $r31
-	setl $r31 aaa
+	add	 $r4 $r2 $r1
+	sub	 $r4 $r2 $r1
+	addi	 $r2 $r1 127
+label:	
+	subi	 $r2 $r1 127
+	fadd	 $f4 $f2 $f1
+	fadda	 $f4 $f2 $f1
+	faddn	 $f4 $f2 $f1
+	fsub	 $f4 $f2 $f1
+	fsuba	 $f4 $f2 $f1
+	fsubn	 $f4 $f2 $f1
+	fmul	 $f4 $f2 $f1
+	fmula	 $f4 $f2 $f1
+	fmuln	 $f4 $f2 $f1
+	finv	 $f4 $f2
+	finva	 $f4 $f2
+	finvn	 $f4 $f2
+	fabs	 $f4 $f2
+	fneg	 $f4 $f2
+	sqrt	 $f4 $f2
+	sqrta	 $f4 $f2
+	sqrtn	 $f4 $f2
+	itof	 $f4 $r2
+	ftoi	 $r4 $f2
+	floor	 $f4 $f2
+	and	 $r4 $r2 $r1
+	or	 $r4 $r2 $r1
+	nor	 $r4 $r2 $r1
+	xor	 $r4 $r2 $r1
+	andi	 $r2 $r1 127
+	ori	 $r2 $r1 127
+	sll	 $r4 $r2 7
+	srl	 $r4 $r2 7
+	sra	 $r4 $r2 7
+	r2r	 $r4 $r2
+	f2f	 $f4 $f2
+	r2f	 $f4 $r2
+	f2r	 $r4 $f2
+	lui	 $r2 $r1 127
+	lli	 $r2 $r1 127
+	flui	 $f2 $f1 127
+	flli	 $f2 $f1 127
+	lw	 $r4 $r2 $r1
+	lwi	 $r2 $r1 127
+	flw	 $f4 $r2 $r1
+	flwa	 $f4 $r2 $r1
+	flwn	 $f4 $r2 $r1
+	flwi	 $r2 $f1 127
+	flwia	 $r2 $f1 127
+	flwin	 $r2 $f1 127
+	sw	 $r4 $r2 $r1
+	swi	 $r2 $r1 127
+	fsw	 $f4 $r2 $r1
+	fswi	 $r2 $f1 127
+	in	 $r4
+	fin	 $f4
+	outa	 $r2
+	outb	 $r2
+	outc	 $r2
+	outd	 $r2
+	fouta	 $f2
+	foutb	 $f2
+	foutc	 $f2
+	foutd	 $f2
+	j	 label
+	jl	 label
+	jr	 $r4
+	jlr	 $r4
+	beq	 $r2 $r1 label
+	beqi	 $r2 15 label
+	fbeq	 $f2 $f1 label
+	bne	 $r2 $r1 label
+	bnei	 $r2 15 label
+	fbne	 $f2 $f1 label
+	blte	 $r2 $r1 label
+	bltei	 $r2 15 label
+	fblte	 $f2 $f1 label
+	bgte	 $r2 $r1 label
+	bgtei	 $r2 15 label
+	fbgte	 $f2 $f1 label
+	beqr	 $r4 $r2 $r1
+	beqir	 $r4 15 label
+	fbeqr	 $r4 $f2 $f1
+	bner	 $r4 $r2 $r1
+	bneir	 $r4 15 label
+	fbner	 $r4 $f2 $f1
+	blter	 $r4 $r2 $r1
+	blteir	 $r4 15 label
+	fblter	 $r4 $f2 $f1
+	bgter	 $r4 $r2 $r1
+	bgteir	 $r2 15 label
+	fbgter	 $r4 $f2 $f1
+	nop	
+	halt	
