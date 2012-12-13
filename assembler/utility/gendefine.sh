ab0769f3c475eef0b3039f153748be86a1755c88
cpumul4 <muroykk@is.s.u-tokyo.ac.jp>
2012-12-13 13:16:35 +0900

    _

--- assembler/utility/gendefine.sh
+++ assembler/utility/gendefine.sh
@@ -3,7 +3,7 @@ awk '{ prefix="#define\t"$1"_"; print ""}
 {printf ("%sformat\t%s\n", prefix, $2) }
 {printf ("%sopcode\t0b%s\n", prefix, $3) }
 {printf ("%sfunct\t0b%s\n", prefix, $5) }
-{if ($8 ~ "[0-1]") printf ("%sfunctR\t0b%s\n", prefix, $8) }
-{if ($7 ~ "[0-1]") printf ("%sfunctR\t0b%s\n", prefix, $7) }
-
+{if ($8 ~ "[0-1]") printf ("%sfunctR\t0b%s\n", prefix, $8);
+else if ($7 ~ "[0-1]")  printf ("%sfunctR\t0b%s\n", prefix, $7);
+else printf ("%sfunctR\t0b0\n", prefix) }
 ' $* | sed -e '/[a-z]$/d'
